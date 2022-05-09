ca5590d8ca1819d1ef97672d23c2f0d85ae7dc4c
WuRuXin <1530484937@qq.com>
2022-05-09 14:10:52 +0800

    Updates

---
+++ precache-manifest.e94b81046aafab4638643294a2bb23a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69b7618645c791288d53",
+    "url": "/LPRopstenTest/static/css/main.53abf2f3.chunk.css"
+  },
+  {
+    "revision": "69b7618645c791288d53",
+    "url": "/LPRopstenTest/static/js/main.69b76186.chunk.js"
+  },
+  {
+    "revision": "1e0049ad3abdc905915b",
+    "url": "/LPRopstenTest/static/css/1.2b13622e.chunk.css"
+  },
+  {
+    "revision": "1e0049ad3abdc905915b",
+    "url": "/LPRopstenTest/static/js/1.1e0049ad.chunk.js"
+  },
+  {
+    "revision": "30344fa2abde047be662",
+    "url": "/LPRopstenTest/static/js/runtime~main.30344fa2.js"
+  },
+  {
+    "revision": "982ef1ec279ebc73fe3a9ba4bae44eda",
+    "url": "/LPRopstenTest/index.html"
+  }
+];
